8d7c5eb0a13308a5e7e6828006c5d3b662b09566
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-07-03 08:02:22 +0530

    hm

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import { websiteRouter } from './routes/website';
 import cors from 'cors'
 import { statusRouter } from './routes/status';
+
 const app = express();
 app.use(cors())
+app.use(express.json())
 
 app.use('/website', websiteRouter);
 app.use('/status', statusRouter);
--- apps/api/src/routes/website.ts
+++ apps/api/src/routes/website.ts
@@ -1,6 +1,6 @@
 import express from "express"
 export const websiteRouter = express.Router()
-import {prismaClient} from "store/client"
+import {prismaClient} from "store"
 
 websiteRouter.post('/', (req, res)=>{
 
