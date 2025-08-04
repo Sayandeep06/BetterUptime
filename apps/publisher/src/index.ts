39536735bedc41e73f74062f22e5415e3a036d2c
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-08-05 00:00:18 +0530

    publisher

--- apps/publisher/src/index.ts
+++ apps/publisher/src/index.ts
@@ -1,1 +1,18 @@
 import {prismaClient} from 'store'
+import { xAdd } from 'redisclient'
+import { xAddAll } from 'redisclient'
+
+async function publisher(){
+    const websites = await prismaClient.website.findMany({
+        select: {
+            url: true,
+            id: true
+        }
+    })
+}
+
+setInterval(() => {
+    publisher()
+}, 3*1000);
+
+publisher();
